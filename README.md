6d3cdf2ec9211cf4427bb6910acdf9703694cdb3
Lars Willighagen <lars.willighagen@gmail.com>
2019-02-10 15:14:41 +0100

    Code, license

--- README.md
+++ README.md
@@ -1,2 +1,15 @@
 # alumade-site-demo
-Demo site for Alumade
+
+Demo site for Alumade. A group of friends were supposed to make a website, and decided PhotoShop was a good option. I could not let that happen.
+
+Then they wanted an interactive 3D model (luckily BabylonJS is great).
+
+
+## License
+
+  * Source code: MIT 2019- Lars Willighagen (see LICENSE file)
+  * [Banner image](https://commons.wikimedia.org/wiki/File:Tahoe_Galaxy.jpg): User:Jarrodlopiccolo [CC BY-SA 3.0](https://creativecommons.org/licenses/by-sa/3.0), from Wikimedia Commons
+  * [BabylonJS](https://www.babylonjs.com/) (not included in repo): [Apache License 2.0](http://www.apache.org/licenses/#2.0)
+    * [BabylonJS STL loader](https://doc.babylonjs.com/how_to/stl): [Apache License 2.0](http://www.apache.org/licenses/#2.0)
+  * [Roboto](https://fonts.google.com/specimen/Roboto) font: [Apache License 2.0](http://www.apache.org/licenses/#2.0)
+  * 3D model: 2019- Alumade
---
+++ assets/js/scene.js
@@ -0,0 +1,39 @@
+const canvas = document.getElementById('model-001')
+const engine = new BABYLON.Engine(canvas, true)
+const scene = new BABYLON.Scene(engine)
+
+const colorMat = new BABYLON.StandardMaterial('color', scene)
+colorMat.diffuseColor = new BABYLON.Color3(1, 1, 1)
+
+scene.createDefaultCameraOrLight(true, true, true)
+
+const camera = new BABYLON.UniversalCamera('UniversalCamera', new BABYLON.Vector3.Zero(), scene)
+camera.setTarget(BABYLON.Vector3.Zero())
+camera.rotation.x = Math.PI / 2
+camera.attachControl(canvas, false)
+
+const light = new BABYLON.PointLight('pointLight', new BABYLON.Vector3(0, 0.25, 0), scene)
+light.diffuse = new BABYLON.Color3(1, 0.75, 0)
+
+BABYLON.SceneLoader.ImportMesh(null, './assets/models/', '001.stl', scene, function (mesh) {
+  window.mesh = mesh
+  mesh.forEach(mesh => {
+    mesh.material = colorMat
+    mesh.position = new BABYLON.Vector3(0, -0.25, 0)
+    mesh.rotation = new BABYLON.Vector3(-Math.PI / 2, 0, 0)
+    mesh.scaling = new BABYLON.Vector3(3e-3, 3e-3, 3e-3)
+  })
+  engine.resize()
+})
+
+engine.runRenderLoop(function () {
+  scene.render()
+})
+
+window.addEventListener('resize', function () {
+  engine.resize()
+})
+
+function setColor (hex) {
+  colorMat.diffuseColor = new BABYLON.Color3(...hex.match(/[0-9a-f]{2}/g).map(num => parseInt(num, 16) / 255))
+}
