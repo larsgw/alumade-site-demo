536146ca3dcc9e3edb75a23ccbf6ea9e86cd6156
Lars Willighagen <lars.willighagen@gmail.com>
2019-03-25 20:35:28 +0100

    Add content

--- assets/js/scene.js
+++ assets/js/scene.js
@@ -18,7 +18,7 @@ camera.attachControl(canvas, false)
 const light = new BABYLON.PointLight('pointLight', new BABYLON.Vector3(0, 0.25, 0), scene)
 light.diffuse = new BABYLON.Color3(1, 0.75, 0)
 
-BABYLON.SceneLoader.ImportMesh(null, './assets/models/', '001.stl', scene, function (mesh) {
+BABYLON.SceneLoader.ImportMesh(null, './assets/models/', window.model, scene, function (mesh) {
   window.mesh = mesh
   mesh.forEach(mesh => {
     mesh.material = colorMat
@@ -26,18 +26,16 @@ BABYLON.SceneLoader.ImportMesh(null, './assets/models/', '001.stl', scene, funct
     mesh.rotation = new BABYLON.Vector3(-Math.PI / 2, 0, 0)
     mesh.scaling = new BABYLON.Vector3(3e-3, 3e-3, 3e-3)
   })
-  
+
   const shadowGenerator = new BABYLON.ShadowGenerator(1024, light)
   shadowGenerator.addShadowCaster(mesh[0])
 
-  const plane = BABYLON.MeshBuilder.CreatePlane('plane', {
-    height: 5,
-    width: 5,
-    sourcePlane: (new BABYLON.Plane(0, 1, 0, -0.3)).normalize(),
+  const box = BABYLON.MeshBuilder.CreateBox('box', {
+    size: 1,
     sideOrientation: BABYLON.Mesh.BACKSIDE
   }, scene)
-  plane.receiveShadows = true
-  plane.material = groundMat
+  box.receiveShadows = true
+  box.material = groundMat
 
   engine.resize()
 })
